70e8886df869e1d063712c1650fb93950f8e0f5b
loin3 <ya6675@naver.com>
2020-04-04 17:48:03 +0900

    final

--- PlayerProgram.py
+++ PlayerProgram.py
@@ -5,16 +5,25 @@ import os
 music_list = get_music_list()
 music_player.initMixer()
 
+is_playing = False
+which_file_is_playing  = ''
+
 while True:
     os.system('cls')
     for i, ml in enumerate(music_list):
         print(str(i) + '.' + ml)
 
+    if is_playing:
+        print('now playing : ' + which_file_is_playing)
+
     user_input = input()
 
     if 0 <= int(user_input) < len(music_list):
         music_player.playmusic(music_list[int(user_input)])
+        is_playing = True
+        which_file_is_playing = music_list[int(user_input)]
     elif user_input == 'stop':
         music_player.stopmusic()
+        is_playing = False
     else:
         exit()
