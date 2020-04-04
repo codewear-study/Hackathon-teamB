6b239458831a5a0b2f57c2162d998202e91f5c7e
loin3 <ya6675@naver.com>
2020-04-04 17:33:37 +0900

    tlwkf

--- PlayerProgram.py
+++ PlayerProgram.py
@@ -1,19 +1,22 @@
 from get_music_list import get_music_list
 import dasda as music_player
+import os
 
 music_list = get_music_list()
 music_player.initMixer()
 
-for i,ml in enumerate(music_list):
-    print(str(i) + '.' + ml)    
-
 while True:
+    for i,ml in enumerate(music_list):
+        print(str(i) + '.' + ml)    
+
     user_input = input()
 
     if user_input.isdigit():
         music_player.playmusic(music_list[int(user_input)])
+        os.system('cls')
     elif user_input == 'stop':
         music_player.stopmusic()
+        os.system('cls')
     
     
 
