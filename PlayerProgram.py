d1e6047d24daf84341a1e0b102b0b53587ff26b0
loin3 <ya6675@naver.com>
2020-04-04 17:13:57 +0900

    tlqkf

--- PlayerProgram.py
+++ PlayerProgram.py
@@ -1,2 +1,26 @@
-def drawList():
+from get_music_list import get_music_list
+import dasda as music_player
+
+music_list = get_music_list()
+
+for i in music_list:
+    print(i + '.' + music_list[i])    
+
+while True:
+    user_input = input()
+
+    if user_input.isdigit:
+        music_player.playmusic(music_list[user_input])
+    elif user_input == 'stop':
+        music_player.stopmusic()
     
+    
+
+
+
+
+
+
+
+
+
