8ec272c4c29b6afa0f103eba79dc213792acdfbc
loin3 <ya6675@naver.com>
2020-04-04 15:41:55 +0900

    Create MusicPlayer.cpp

---
+++ MusicPlayer.cpp
@@ -0,0 +1,15 @@
+class MusicPlayer{
+    string filePath;
+
+    public:
+        int playMusic(filePath);
+        int stopMusic(filePath);
+}
+
+class MusicList{
+    List musicList;
+
+    public:
+        List getMusicList();
+}
+
